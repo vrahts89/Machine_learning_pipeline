0c94d86e26c069cdf2f8f6aa4f93aca15a69381b
vrahts89 <76011433+vrahts89@users.noreply.github.com>
2024-01-21 21:28:23 +0100

    added process data file to data folder

---
+++ data/process_data.py
@@ -0,0 +1,132 @@
+# import libraries
+import sys
+import requests
+import pandas as pd
+import numpy as np
+import sqlite3
+from sqlalchemy import create_engine
+
+def load_data(messages_filepath, categories_filepath):
+    '''
+    load_data
+    load data from csv files and merge to a single pandas dataframe
+    
+    Input:
+    message_filepath filepath to messsages csv file
+    categories_filepath filepath to categories csv file
+    
+    Returns:
+    df  dataframe merging categories and messages
+    '''
+    
+    # load messages dataset
+    messages = pd.read_csv(messages_filepath)
+
+    # load categories dataset
+    categories = pd.read_csv(categories_filepath)
+
+    # merge datasets
+    df = pd.merge(messages, categories, on="id")
+    
+    #return the merge dataframe
+    return df
+
+def clean_data(df):
+    '''
+    clean_data
+    cleans the merged dataset
+    
+    Input:
+    df  dataframe with the merged data
+    
+    Returns:
+    df  cleaned dataframe
+    '''
+    # create a dataframe of the 36 individual category columns
+    categories = df["categories"].str.split(pat=";", expand=True)
+
+    # select the first row of the categories dataframe
+    row = categories.iloc[0]
+
+    # use this row to extract a list of new column names for categories.
+    # lambda function is applied that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.apply(lambda x: x.split("-")[0])
+
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+
+    # change datatype of columns to string
+    categories = categories.astype(str)
+
+    for column in categories:
+        # set each value to be the last character of the string
+        # convert column from string to int
+        categories[column] = categories[column].str[-1].astype(int)
+
+    # drop the original categories column from `df`
+    df = df.drop(columns="categories", axis=1)
+
+    # concatenate the original dataframe with the new `categories` dataframe
+    df = pd.concat([df, categories], axis=1)
+
+    # drop duplicates
+    df = df.drop_duplicates()
+
+    df = df.drop(columns=["original"])
+
+    #remove null values
+    df = df.dropna()
+
+    #check if the values in the columns are only 1s and 0s
+    for column in df.columns:
+        print(df[column].value_counts())
+
+    #filter out the rows with value 2
+    df = df[df['related'] != 2]
+    
+    #return the cleaned dataframe
+    return df
+
+
+def save_data(df, database_filename):
+    '''
+    save_data
+    saves the cleaned dataframe to an sqlite database
+    
+    Input:
+    df  dataframe with the cleaned data
+    '''
+    #save clean dataset into sqlite database
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql("disaster_response", engine, index=False, if_exists='replace')
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
